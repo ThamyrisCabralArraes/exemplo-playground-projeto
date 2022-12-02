97440b79c5d7bde41e395574d23afa087a5fbb36
Thamyris <thamyriscabral@hotmail.com>
2022-12-02 14:22:44 -0300

    Feito requisitos: 10

---
+++ 10-requisito.js
@@ -0,0 +1,25 @@
+const arrayTech = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+const techList = (array, stryng) => {
+  let techObject = [];
+
+  if (array.length >= 5) {
+    array = array.sort();
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (index < array.length) {
+      techObject.push({
+        tech: array[index],
+        name: stryng,
+      });
+    } else {
+      techObject.push({
+        tech: '',
+        name: '',
+      });
+    }
+  }
+
+  return techObject;
+};
+console.log(techList(arrayTech, 'Thamyris'));
